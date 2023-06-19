043df53a99c083a0de83c1202fe49e76c81f9a30
JesusXD88 <71974251+JesusXD88@users.noreply.github.com>
2023-06-20 00:31:40 +0200

    Added WiFi settings

--- src/server.py
+++ src/server.py
@@ -1,6 +1,10 @@
 import scripts
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import nmcli
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -17,6 +21,46 @@ def reboot():
   print ('Rebooting System!')
 
   return scripts.reboot()
+
+@app.route('/wifi/')
+def wifi_settings():
+    info = nmcli.device.show_all()
+    for connection in info:
+      if connection.get('GENERAL.DEVICE') == 'wlo1':
+          connection_info = connection
+          break
+    
+    conn_name = None
+    ip_address = None
+    if connection_info.get('GENERAL.STATE') == '100 (connected)':
+      conn_name = connection_info.get('GENERAL.CONNECTION')
+      ip_address = connection_info.get('IP4.ADDRESS[1]')
+
+    networks = nmcli.device.wifi()
+    return render_template('wifi_settings.html', networks=networks, conn_name=conn_name, ip_address=ip_address)
+
+@app.route('/connect', methods=['POST'])
+def connect():
+    try:
+      ssid = request.form.get('ssid')
+      password = request.form.get('password')
+      nmcli.device.wifi_connect(ssid, password)
+      connection_info = nmcli.connection.show(ssid)
+      ip_address = connection_info.get('IP4.ADDRESS[1]')
+      return redirect(url_for('success', ssid=ssid, ip_address=ip_address))
+    except Exception as e:
+      print(f'Error: {str(e)}')
+      return redirect(url_for('failure'))
+
+@app.route('/success')
+def success():
+    ssid = request.args.get('ssid')
+    ip_address = request.args.get('ip_address')
+    return render_template('success.html', ssid=ssid, ip_address=ip_address)
+
+@app.route('/failure')
+def failure():
+    return render_template('failure.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
