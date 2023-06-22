51187c301657938850e150c054d7c3fc7395eee4
JesusXD88 <71974251+JesusXD88@users.noreply.github.com>
2023-06-22 21:08:14 +0200

    Add option to open Dolphin-Emu

--- src/scripts.py
+++ src/scripts.py
@@ -1,8 +1,13 @@
+import os
+from Xlib import display, X
 import subprocess
 import signal
 import time
 
+dolphin_process = None
+
 def end_dolphin():
+    print("Shutting down dolphin!")
     program = "dolphin"
     pids = subprocess.check_output(["pgrep", program]).splitlines()
     if len(pids) >= 2:
@@ -21,3 +26,46 @@ def reboot():
     time.sleep(10)
     subprocess.run(["/usr/bin/systemctl", "reboot"], check=True)
     return 0
+
+def open_x11_application(application_command):
+    print("Opening application: " + application_command)
+    disp = display.Display(os.environ['DISPLAY'])
+    print(disp)
+    root = disp.screen().root
+
+    win = root.create_window(
+        0, 0, 1, 1,
+        0,
+        disp.screen().root_depth,
+        X.InputOutput,
+        X.CopyFromParent,
+        event_mask=X.ExposureMask | X.KeyPressMask
+    )
+
+    win.map()
+
+    while 1:
+        event = disp.next_event()
+        if event.type == X.Expose:
+            break
+
+    import subprocess
+    subprocess.Popen(application_command, shell=True)
+
+    disp.close()
+
+    return ''
+
+def open_and_run_dolphin():
+    global dolphin_process
+
+    if dolphin_process is not None and dolphin_process.poll() is None:
+        return "Dolphin is already running"
+
+    print('Opening dolphin!')
+    dolphin_process = subprocess.Popen(["dolphin-emu"])
+    dolphin_process.wait()
+
+    print('Running dolphin command!')
+    subprocess.run(["gamemoderun", "mangohud", "dolphin-emu", "-b", "-n", "0000000100000002"])
+    return "Command executed successfully"
--- src/server.py
+++ src/server.py
@@ -10,59 +10,63 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/poweroff/')
+@app.route('/poweroff')
 def poweroff():
   print ('Shutting down System!')
-
   return scripts.poweroff()
 
-@app.route('/reboot/')
+@app.route('/reboot')
 def reboot():
   print ('Rebooting System!')
-
   return scripts.reboot()
 
-@app.route('/wifi/')
+@app.route('/wifi')
 def wifi_settings():
-    info = nmcli.device.show_all()
-    connection_info = None
-    for connection in info:
-      if connection.get('GENERAL.DEVICE') == 'wlo1':
-          connection_info = connection
-          break
-    
-    conn_name = None
-    ip_address = None
-    if connection_info != None:
-      if connection_info.get('GENERAL.STATE') == '100 (connected)':
-        conn_name = connection_info.get('GENERAL.CONNECTION')
-        ip_address = connection_info.get('IP4.ADDRESS[1]')
+  info = nmcli.device.show_all()
+  connection_info = None
+  for connection in info:
+    if connection.get('GENERAL.DEVICE') == 'wlo1':
+        connection_info = connection
+        break
+  
+  conn_name = None
+  ip_address = None
+  if connection_info != None:
+    if connection_info.get('GENERAL.STATE') == '100 (connected)':
+      conn_name = connection_info.get('GENERAL.CONNECTION')
+      ip_address = connection_info.get('IP4.ADDRESS[1]')
 
-    networks = nmcli.device.wifi()
-    return render_template('wifi_settings.html', networks=networks, conn_name=conn_name, ip_address=ip_address)
+  networks = nmcli.device.wifi()
+  return render_template('wifi_settings.html', networks=networks, conn_name=conn_name, ip_address=ip_address)
 
 @app.route('/connect', methods=['POST'])
 def connect():
-    try:
-      ssid = request.form.get('ssid')
-      password = request.form.get('password')
-      nmcli.device.wifi_connect(ssid, password)
-      connection_info = nmcli.connection.show(ssid)
-      ip_address = connection_info.get('IP4.ADDRESS[1]')
-      return redirect(url_for('success', ssid=ssid, ip_address=ip_address))
-    except Exception as e:
-      print(f'Error: {str(e)}')
-      return redirect(url_for('failure'))
+  try:
+    ssid = request.form.get('ssid')
+    password = request.form.get('password')
+    nmcli.device.wifi_connect(ssid, password)
+    connection_info = nmcli.connection.show(ssid)
+    ip_address = connection_info.get('IP4.ADDRESS[1]')
+    return redirect(url_for('success', ssid=ssid, ip_address=ip_address))
+  except Exception as e:
+    print(f'Error: {str(e)}')
+    return redirect(url_for('failure'))
 
 @app.route('/success')
 def success():
-    ssid = request.args.get('ssid')
-    ip_address = request.args.get('ip_address')
-    return render_template('success.html', ssid=ssid, ip_address=ip_address)
+  ssid = request.args.get('ssid')
+  ip_address = request.args.get('ip_address')
+  return render_template('success.html', ssid=ssid, ip_address=ip_address)
 
 @app.route('/failure')
 def failure():
-    return render_template('failure.html')
+  return render_template('failure.html')
+
+@app.route('/open_dolphin')
+def open_dolphin():
+  print ('Opening dolphin!')
+  scripts.end_dolphin()
+  return scripts.open_and_run_dolphin()
 
 if __name__ == '__main__':
   app.run(debug=True)
