0e112366a15ebe69e2455d41576becebbe3ea844
JesusXD88 <71974251+JesusXD88@users.noreply.github.com>
2023-06-22 19:04:39 +0200

    Set to wait 10 seconds after ending emu before shutdown

--- src/scripts.py
+++ src/scripts.py
@@ -1,9 +1,23 @@
-import os
+import subprocess
+import signal
+import time
+
+def end_dolphin():
+    program = "dolphin"
+    pids = subprocess.check_output(["pgrep", program]).splitlines()
+    if len(pids) >= 2:
+        pid = int(pids[1])
+        child_pid = int(pids[0])
+        os.kill(pid, signal.SIGTERM)
 
 def poweroff():
-    os.system("systemctl poweroff")
+    end_dolphin()
+    time.sleep(10)
+    subprocess.run(["/usr/bin/systemctl", "poweroff"], check=True)
     return 0
 
 def reboot():
-    os.system("systemctl reboot")
+    end_dolphin()
+    time.sleep(10)
+    subprocess.run(["/usr/bin/systemctl", "reboot"], check=True)
     return 0
